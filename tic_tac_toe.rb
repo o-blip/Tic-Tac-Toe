4a2c26ee3c8236b2e4ae57a746ad16b687753147
piero <dolibiroli@gmail.com>
2022-03-23 11:13:26 -0500

    simplified Board class, added tie case but need to finish it

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,30 +1,40 @@
 # Represents the game board
 class Board
+  attr_accessor :board
+
   def initialize
-    @board = Array.new(3, ' ') { Array.new(3, ' ') }
+    @board = Array.new(9, ' ')
   end
 
   def mark_board(mark, number)
-    row = (number - 1) / 3
-    column = (number - 1) % 3
-    @board[row][column] = mark
+    return 'nope' if self.board[number - 1] != ' '
+
+    self.board[number - 1] = mark
   end
 
   def print_board
-    puts "#{@board[0][0]}|#{@board[0][1]}|#{@board[0][2]}"
-    puts "#{@board[1][0]}|#{@board[1][1]}|#{@board[1][2]}"
-    puts "#{@board[2][0]}|#{@board[2][1]}|#{@board[2][2]}"
+    puts "#{self.board[0]}|#{self.board[1]}|#{self.board[2]}"
+    puts "#{self.board[3]}|#{self.board[4]}|#{self.board[5]}"
+    puts "#{self.board[6]}|#{self.board[7]}|#{self.board[8]}"
   end
 
   def winner?
-    possibilities = [[@board[0][0], @board[1][1], @board[2][2]], [@board[0][2], @board[1][1], @board[2][0]]]
-    for i in (0..2)
-      possibilities.push(@board[i])
-      possibilities.push([@board[0][i], @board[1][i], @board[2][i]])
-    end
+    possibilities = [[self.board[0], self.board[4], self.board[8]], [self.board[2], self.board[4], self.board[6]]] # Diagonals
+    possibilities.concat(self.rows)
+    possibilities.concat(self.columns)
+    return 'tie' if possibilities.flatten.none?(' ') && possibilities.none? { |poss| poss.all?('x') || poss.all?('o') }
+
     possibilities.any? { |poss| poss.all?('x') || poss.all?('o') }
   end
 
+  private
+  def rows
+    (0..2).map { |i| [self.board[3 * i], self.board[3 * i + 1], self.board[3 * i + 2]] }
+  end
+
+  def columns
+    (0..2).map { |i| [self.board[i], self.board[i + 3], self.board[i + 6]] }
+  end
 end
 
 # Player objects which choose where to place marks
@@ -40,14 +50,31 @@ class Player
   end
 
   def choose_move(board, number)
+    # checking for choosing same spot
+    while board.mark_board(self.mark, number) == 'nope'
+      puts 'try again'
+      number = gets.chomp.to_i
+    end
+
     board.mark_board(self.mark, number)
     self.next = false
   end
+
   def next?
     @next
   end
 end
 
+def new_round(answer)
+  if answer == 'y'
+    board = Board.new
+    first_round = true
+  else
+    still_playing = false
+  end
+end
+
+############## main script ##############
 still_playing = true
 
 puts 'Enter player name:'
@@ -65,17 +92,18 @@ while still_playing
     if rand(2).zero?
       player1.mark = 'x'
       player1.next = true
-      puts "#{player1.name} goes first:"
+      puts "#{player1.name} goes first."
     else
       player2.mark = 'x'
       player2.next = true
-      puts "#{player2.name} goes first:"
+      puts "#{player2.name} goes first."
     end
     first_round = false
   end
 
-  puts 'MAKE YOUR MOVE FOOL:'
   puts "1|2|3\n4|5|6\n7|8|9"
+  print player1.next? ? player1.name : player2.name
+  puts ", your move:\n\n"
 
   if player1.next?
     player1.choose_move(board, gets.chomp.to_i)
@@ -90,11 +118,9 @@ while still_playing
   if board.winner?
     puts player1.next? ? "Winner is #{player2.name}" : "Winner is #{player1.name}"
     puts 'Play again? y/n:'
-    if gets.chomp.downcase == 'y'
-      board = Board.new
-      first_round = true
-    else
-      still_playing = false
-    end
+    new_round(gets.chomp.to_i)
+  elsif board.winner? == 'tie'
+    puts 'Tie!,Play again? y/n:'
+    new_round(gets.chomp.to_i)
   end
 end
